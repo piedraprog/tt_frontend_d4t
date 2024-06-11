5328e3cb3ebf3006b4b6664b32605102a2c8271c
drapie <j.piedraprog@gmail.com>
2024-06-11 13:52:35 -0400

    fix: testing routes with the cookie

--- src/app/data/actions/outh-actions.ts
+++ src/app/data/actions/outh-actions.ts
@@ -11,7 +11,7 @@ import {
 const config = {
   maxAge: 60 * 60 * 24 * 7, // 1 week
   path: "/",
-  domain: process.env.HOST ?? "localhost",
+  domain: "develop.botcito.dev" ?? "localhost",
   httpOnly: true,
   secure: process.env.NODE_ENV === "production",
 };
@@ -64,7 +64,8 @@ export async function registerUserAction(prevState: any, formData: FormData) {
     };
   }
 
-  cookies().set("jwt", responseData.jwt, config);
+  cookies().set() 
+  // ("jwt", responseData.jwt, config);
   redirect("/dashboard");
 }
 
