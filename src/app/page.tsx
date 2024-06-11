19ce195e9e74f7c776bc255082245c5541c7df43
drapie <j.piedraprog@gmail.com>
2024-06-11 13:31:42 -0400

    test: fix route

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,15 +6,14 @@ import Link from "next/link";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Link href={"/login"}>
+      <Link href={"/signin"}>
         <Button
           className="mt-4 w-40"
           variant="secondary"
         >
-          Login
+          Sign In Develop
         </Button>
       </Link>
-      <h2>Basic </h2>
     </main>
   );
 }
