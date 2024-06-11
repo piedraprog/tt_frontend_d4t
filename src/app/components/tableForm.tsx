9c691b6869ea70e80834cc0d9d721d746d5fe759
drapie <j.piedraprog@gmail.com>
2024-06-11 08:58:18 -0400

    fix: import error fixed

--- src/app/components/tableForm.tsx
+++ src/app/components/tableForm.tsx
@@ -11,7 +11,7 @@ import { Button } from '@/components/ui/button';
 import { useRouter } from 'next/navigation';
 import { Episodes } from '../utils/interfaces/episodes.interface';
 import { Badge } from "@/components/ui/badge"
-import CharacterHoverCard from './hoverCharCard';
+// import CharacterHoverCard from './hoverCharCard';
 
 
 interface TableProps<T> {
